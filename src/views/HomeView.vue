01e23fca149ebf5bbc43d4acb313dad19646d064
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-23 12:12:33 -0300

    Props and lifecycle hooks in setup function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,28 @@
 <template>
   <div class="home">
    <h1>Home</h1>
-   <input type="text" v-model="search">
-   <p>Search term - {{ search }}</p>
-   <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-   <button @click="handleClick">stop watching</button>
-   
+   <PostList v-if="showPosts" :posts="posts"/>
+   <button @click="showPosts = !showPosts">toggle posts</button>
+   <button @click="posts.pop()">delete a post</button>
+
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    //This function doesn't run initialy when the componet first runs
-    //If you dont use a value inside the function this the best option
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    //This function runs initialy when the componet first runs
-    //Use this function if you want to use a value inside the function
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffectt function ran', search.value)  
-    })
-
-    // This computed property returns updated value based on other values (in this case, const names array)
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
+    const posts = ref([
+      {title: 'welcome to my blog', body: 'In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Duis sapien nunc, commodo et, interdum suscipit, sollicitudin et, dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Mauris dictum facilisis augue. Fusce tellus. Pellentesque arcu. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Etiam ligula pede, sagittis quis, interdum ultricies,', id: 1},
+      {title: 'top 5 css tips', body: 'In sem justo, commodo', id: 2}
+    ])
+    const showPosts = ref(true)
     
-    return { names, search, matchingNames, handleClick }
+    return { posts, showPosts }
 
   }
 }
