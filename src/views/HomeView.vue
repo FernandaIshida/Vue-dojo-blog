918853306e9776cf73c3d71525aeaddb1e127bc1
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-23 10:51:50 -0300

    Watch and WatchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,13 +9,25 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { computed, ref, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
   setup() {
     const search = ref('')
     const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+
+    //This function doesn't run initialy when the componet first runs
+    //If you dont use a value inside the function this the best option
+    watch(search, () => {
+      console.log('watch function ran')
+    })
+
+    //This function runs initialy when the componet first runs
+    //Use this function if you want to use a value inside the function
+    watchEffect(() => {
+      console.log('watchEffectt function ran', search.value)  
+    })
 
     // This computed property returns updated value based on other values (in this case, const names array)
     const matchingNames = computed(() => {
