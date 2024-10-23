8c40ca389e79ebba0707c02da83e385a854aa70b
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-23 09:07:55 -0300

    Composition APIs, template refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   Home
+   <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+   <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    console.log(this)
+
+    const p = ref(null)
+    console.log(p, p.value)
+
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'Hello, ninjas'
+    }
+
+    return { name, age, handleClick, p }
   }
 }
 </script>
